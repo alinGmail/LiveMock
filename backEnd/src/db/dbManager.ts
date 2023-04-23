b615d9cfa778e59604806e934a8c6d3c4a4fde79
dixiaolin <a2231232@gmail>
2023-04-23 22:54:52 +0800

    init project

--- backEnd/src/db/dbManager.ts
+++ backEnd/src/db/dbManager.ts
@@ -2,7 +2,7 @@ import Datastore from "nedb";
 import PromiseDatastore from "./promiseDatastore";
 import { ProjectM } from "core/struct/project";
 import { ExpectationM } from "core/struct/expectation";
-
+import { LogM } from "core/struct/log";
 
 let projectDb = new Datastore({ filename: "db/project.db", autoload: true });
 let projectDbP = new PromiseDatastore<ProjectM>(projectDb);
@@ -21,7 +21,9 @@ export function getExpectationDb(
       filename: `db/${projectName}_Exp.db`,
       autoload: true,
     });
-    let newExpectationDbP = new PromiseDatastore<ExpectationM>(newExpectationDb);
+    let newExpectationDbP = new PromiseDatastore<ExpectationM>(
+      newExpectationDb
+    );
     expectationDbMap.set(projectName, newExpectationDbP);
     return newExpectationDbP;
   } else {
@@ -29,10 +31,19 @@ export function getExpectationDb(
   }
 }
 
-const requestLogDbMap = new Map<string,PromiseDatastore<RequestLogM>>();
+const logDbMap = new Map<string, PromiseDatastore<LogM>>();
 
-export function getRequestLogDb(){
-
+export function getLogDb(projectName: string): PromiseDatastore<LogM> {
+  const logDbP = logDbMap.get(projectName);
+  if (!logDbP) {
+    const newLogDb = new Datastore({
+      filename: `db/${projectName}_Log.db`,
+      autoload: true,
+    });
+    let newLogDbP = new PromiseDatastore<LogM>(newLogDb);
+    logDbMap.set(projectName, newLogDbP);
+    return newLogDbP;
+  } else {
+    return logDbP;
+  }
 }
-
-
