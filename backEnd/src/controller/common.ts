20e7e32ebf0587778bc132eb02cd329f4414699c
dixiaolin <a2231232@gmail>
2023-04-27 21:52:19 +0800

    add projectController

---
+++ backEnd/src/controller/common.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+export function addCross(res: express.Response) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PATCH, PUT, DELETE"
+  );
+  res.header("Allow", "GET, POST, PATCH, OPTIONS, PUT, DELETE");
+}
---
+++ backEnd/src/controller/projectController.ts
@@ -0,0 +1,64 @@
+import express, { Request } from "express";
+import { addCross } from "./common";
+let router = express.Router();
+import { getProjectDb } from "../db/dbManager";
+import bodyParser from "body-parser";
+import { ProjectM } from "core/struct/project";
+
+const projectDbP = getProjectDb();
+
+router.options("*", (req, res) => {
+  addCross(res);
+  res.end();
+});
+
+/**
+ * get all project
+ */
+router.get("/", async (req, res) => {
+  addCross(res);
+  let projects = await projectDbP.findPromise({});
+  res.end(JSON.stringify(projects));
+});
+
+/**
+ * create project
+ */
+router.post(
+  "/",
+  bodyParser.json(),
+  async (req: Request<{}, {}, { project: ProjectM }>, res) => {
+    addCross(res);
+    if (req.body.project) {
+      await projectDbP.insertPromise(req.body.project);
+    }
+    res.end(JSON.stringify(req.body.project));
+  }
+);
+
+/**
+ * update project
+ */
+router.put(
+  "/:projectId",
+  bodyParser.json(),
+  async (
+    req: Request<{ projectId: number }, {}, { updateQuery: any }>,
+    res
+  ) => {
+    addCross(res);
+    await projectDbP.updatePromise(
+      { _id: req.params.projectId },
+      req.body.updateQuery
+    );
+    res.end('success');
+  }
+);
+
+
+router.delete("/:projectId", bodyParser.json(), async (req, res) => {
+  addCross(res);
+  // todo remove expectation
+  // todo remove log
+  res.end('success');
+});
