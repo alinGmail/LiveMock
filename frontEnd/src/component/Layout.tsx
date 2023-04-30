0661949ee36c82d7f501a26a5a5f825a487bf0e2
alin <a2231232@gmail.com>
2023-04-30 09:55:16 +0800

    front ui project name

--- frontEnd/src/component/Layout.tsx
+++ frontEnd/src/component/Layout.tsx
@@ -1,15 +1,21 @@
 import mStyle from "./Layout.module.scss";
 import LeftNav from "./LeftNav";
 import { HashRouter } from "react-router-dom";
+import ProjectInfo from "./ProjectInfo";
 
 const Layout = () => {
   return (
     <HashRouter>
       <div className={mStyle.layout}>
-        <div className={mStyle.leftCol}>
-          <LeftNav />
+        <div className={mStyle.headRow}>
+          <ProjectInfo />
         </div>
-        <div className={mStyle.rightCol}>bbb</div>
+        <div className={mStyle.mainRow}>
+          <div className={mStyle.leftCol}>
+            <LeftNav />
+          </div>
+          <div className={mStyle.rightCol}>bbb</div>
+        </div>
       </div>
     </HashRouter>
   );
---
+++ frontEnd/src/component/ProjectInfo.tsx
@@ -0,0 +1,31 @@
+import mStyle from "./ProjectInfo.module.scss";
+import { ReactComponent as StartIcon } from "../assets/svg/play2.svg";
+import StopIcon from "../svg/stop.svg";
+import Icon, { PlusOutlined } from "@ant-design/icons";
+
+const ProjectInfo = () => {
+  return (
+    <div className={mStyle.projectInfo}>
+      <div className={mStyle.curProjectRow}>
+        <div className={mStyle.rowLeft}>
+          <div className={mStyle.projectName}>test project name</div>
+          <div className={mStyle.projectStatus}>running on port 8080</div>
+        </div>
+        <div className={mStyle.rowRight}>
+          <Icon
+            component={StartIcon}
+            style={{
+              color: "#98FF98",
+              fontSize: "36px",
+            }}
+            className={mStyle.startIcon}
+            onClick={(event) => {}}
+            spin={false}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectInfo;
