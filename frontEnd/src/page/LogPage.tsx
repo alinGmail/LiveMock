0d463a78e4bcf4ee0c490461eca71f621e2cd7bc
alin <a2231232@gmail.com>
2023-06-04 13:06:37 +0800

    log table

--- frontEnd/src/page/LogPage.tsx
+++ frontEnd/src/page/LogPage.tsx
@@ -4,15 +4,43 @@ import {createSimpleFilter, LogM} from "core/struct/log";
 import LogFilterComponent from "../component/log/LogFilterComponent";
 import {io, Socket} from "socket.io-client";
 import {useAppSelector} from "../store";
+import {useDispatch} from "react-redux";
+import {Table} from "antd";
+import {configColumn, getCustomColumn, getDefaultColumn} from "./LogPageColumn";
 
 
-const pageId:string = uuId();
+// const pageId:string = uuId();
 
 const LogPage:React.FC = ()=>{
+
+
+    const logState = useAppSelector(state => state.log);
+    let {
+        columnConfigShow,
+        columnEditorShow,
+        currentColumnEditIndex,
+        defaultColumnVisible,
+        logFilter,
+        logList,
+        tableColumns
+    } = logState;
+    const dispatch = useDispatch();
+
     const projectState = useAppSelector((state) => state.project);
     const currentProject = projectState.projectList[projectState.curProjectIndex];
     const [socketInstance,setSocketInstance] = useState<Socket|null>(null);
-    const [logs,setLogs] = useState<Array<LogM>>([])
+    const [logs,setLogs] = useState<Array<LogM>>([]);
+
+    const customColumns = getCustomColumn(
+        tableColumns.filter((item, index) => item.visible),
+        dispatch
+    );
+    const logColumn = getDefaultColumn(dispatch)
+        .filter((item, index) => defaultColumnVisible[index])
+        .concat(customColumns)
+        .concat(configColumn);
+
+
     useEffect(() =>{
         const socket = io("http://localhost:9002",{
             query:{
@@ -38,7 +66,14 @@ const LogPage:React.FC = ()=>{
     const simpleFilterM = createSimpleFilter();
     return <div>
         <LogFilterComponent filter={simpleFilterM} />
-
+        <div >
+            <Table
+                columns={logColumn}
+                dataSource={logs}
+                size={"small"}
+                tableLayout={"fixed"}
+            />
+        </div>
     </div>
 }
 
