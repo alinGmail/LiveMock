3347999c7b305a7a828fd1ed238cf0fee70d9aa2
alin <a2231232@gmail.com>
2023-07-01 09:28:47 +0800

    tsconfig change

--- frontEnd/vite.config.ts
+++ frontEnd/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(),react()],
+  plugins: [svgr(),react(),tsconfigPaths()]
 })
