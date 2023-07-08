61eacec2f6d834d2ce03cff27d4462d6789e54b8
alin <a2231232@gmail.com>
2023-07-08 08:53:11 +0800

    add text column

--- frontEnd/src/component/table/TextColumn.tsx
+++ frontEnd/src/component/table/TextColumn.tsx
@@ -1,21 +1,35 @@
 import { Component } from "react";
+import { Tooltip } from "antd";
 
 class TextColumn extends Component<{ content: string }, {}> {
   render() {
     return (
-      <div
-        style={{
-          lineClamp: "5",
-          WebkitLineClamp: "5",
-          wordBreak: "break-all",
-          WebkitBoxOrient: "vertical",
-          textOverflow: "ellipsis",
-          display: "-webkit-box",
-          overflow: "hidden",
+      <Tooltip
+        mouseEnterDelay={1}
+        mouseLeaveDelay={0.3}
+        overlayStyle={{ width: "600px", height: "500px", maxWidth: "600px" }}
+        placement={"bottom"}
+        overlayInnerStyle={{
+          width: "600px",
+          height: "500px",
+          overflow: "scroll",
         }}
+        title={this.props.content}
       >
-        {this.props.content}
-      </div>
+        <div
+          style={{
+            lineClamp: "5",
+            WebkitLineClamp: "5",
+            wordBreak: "break-all",
+            WebkitBoxOrient: "vertical",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            overflow: "hidden",
+          }}
+        >
+          {this.props.content}
+        </div>
+      </Tooltip>
     );
   }
 }
