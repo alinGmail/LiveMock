bad9e5158cf5690bf60d2d9fe873b31e075d56f4
alin <a2231232@gmail.com>
2023-08-02 22:19:51 +0800

    add proxy header

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Provider} from "react-redux";
-import {store} from "./store.ts";
+import {store} from "./store";
 
 const queryClient = new QueryClient({
     defaultOptions:{
