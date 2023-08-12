dbd8a57630c335430b97c7d2e8beaf0248cbb004
alin <a2231232@gmail.com>
2023-08-12 22:30:30 +0800

    desktop add support for dark mode

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -30,39 +30,43 @@ function App() {
   useEffect(() => {
     if (systemConfigState.mode === "dark") {
       document.body.className = "dark_mode";
+      document.getElementsByTagName("html")[0].style.background =
+        "linear-gradient(to bottom, #262626 0, #262626 73px, #595959 73px) repeat-x";
     } else {
+      document.getElementsByTagName("html")[0].style.background =
+        "linear-gradient(to bottom, rgb(36, 41, 47) 0, rgb(36, 41, 47) 73px, white 73px) repeat-x";
       document.body.className = "";
     }
   }, [systemConfigState.mode]);
   return (
     <>
-        <ConfigProvider
-          theme={
-            systemConfigState.mode === "dark"
-              ? { algorithm: theme.darkAlgorithm }
-              : {}
-          }
-        >
-          {!projectListQuery.isLoading ? (
-            projectList.length === 0 ? (
-              <WelcomePage />
-            ) : (
-              <Layout>
-                <Routes>
-                  <Route path={"expectation"} element={<ExpectationPage />} />
-                  <Route path={"requestLog"} element={<LogPage />}></Route>
-                  <Route path={"config"} element={<ConfigPage />} />
-                  <Route path={"*"} element={<Navigate to={"expectation"} />} />
-                </Routes>
-              </Layout>
-            )
+      <ConfigProvider
+        theme={
+          systemConfigState.mode === "dark"
+            ? { algorithm: theme.darkAlgorithm }
+            : {}
+        }
+      >
+        {!projectListQuery.isLoading ? (
+          projectList.length === 0 ? (
+            <WelcomePage />
           ) : (
-            <Spin tip="Loading" size="large">
-              <div className="content" style={{ height: "500px" }} />
-            </Spin>
-          )}
-          <Toaster />
-        </ConfigProvider>
+            <Layout>
+              <Routes>
+                <Route path={"expectation"} element={<ExpectationPage />} />
+                <Route path={"requestLog"} element={<LogPage />}></Route>
+                <Route path={"config"} element={<ConfigPage />} />
+                <Route path={"*"} element={<Navigate to={"expectation"} />} />
+              </Routes>
+            </Layout>
+          )
+        ) : (
+          <Spin tip="Loading" size="large">
+            <div className="content" style={{ height: "500px" }} />
+          </Spin>
+        )}
+        <Toaster />
+      </ConfigProvider>
     </>
   );
 }
