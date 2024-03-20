3dc19f7edae16c3b9d765eee54c242c23fbc84d8
alin <a2231232@gmail.com>
2024-03-20 22:38:30 +0800

    fix monaco bug

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -6,9 +6,33 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Provider} from "react-redux";
 import {store} from "./store";
 import { loader } from '@monaco-editor/react';
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
-loader.config({ paths: { vs: '/vs' } });
-
+// loader.config({ paths: { vs: '/vs' } });
+self.MonacoEnvironment = {
+    getWorker(_, label) {
+        if (label === 'json') {
+            return new jsonWorker();
+        }
+        if (label === 'css' || label === 'scss' || label === 'less') {
+            return new cssWorker();
+        }
+        if (label === 'html' || label === 'handlebars' || label === 'razor') {
+            return new htmlWorker();
+        }
+        if (label === 'typescript' || label === 'javascript') {
+            return new tsWorker();
+        }
+        return new editorWorker();
+    },
+};
+loader.config({ monaco });
+loader.init().then(/* ... */);
 
 const queryClient = new QueryClient({
     defaultOptions:{
