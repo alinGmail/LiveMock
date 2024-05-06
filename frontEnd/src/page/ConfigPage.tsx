89485f68cf451decc7c5ab6467fbdf395e4fec13
alin <a2231232@gmail.com>
2024-05-06 22:23:37 +0800

    add support for delete project

--- frontEnd/src/page/ConfigPage.tsx
+++ frontEnd/src/page/ConfigPage.tsx
@@ -4,12 +4,17 @@ import { useImmer } from "use-immer";
 import { useAppSelector } from "../store";
 import { useDebounceEffect, useDebounceFn } from "ahooks";
 import { debounceWait } from "../config";
-import {deleteProjectReq, getProjectListReq, updateProjectReq} from "../server/projectServer";
+import {
+  deleteProjectReq,
+  getProjectListReq,
+  updateProjectReq,
+} from "../server/projectServer";
 import { toastPromise } from "../component/common";
 import { setProjectList } from "../slice/projectSlice";
 import { useDispatch } from "react-redux";
-import { useEffect, useRef } from "react";
-import {Button} from "antd";
+import { useEffect, useRef, useState } from "react";
+import { Button, Input, Modal } from "antd";
+import { message } from "antd/lib";
 
 const ConfigPage = () => {
   const dispatch = useDispatch();
@@ -41,23 +46,68 @@ const ConfigPage = () => {
     [modifyProject],
     {
       wait: debounceWait,
-    }
+    },
   );
 
+  const [messageApi, messageContextHolder] = message.useMessage();
+  const [projectNameInp, setProjectNameInp] = useState<string>("abc");
+  const [deleteModalShow, setDeleteModalShow] = useState(false);
+
   return (
-    <div style={{padding:"20px 10px"}}>
+    <div style={{ padding: "20px 10px" }}>
+      {messageContextHolder}
+      <Modal
+        title={"warning"}
+        open={deleteModalShow}
+        onOk={() => {
+          if (projectNameInp !== currentProject.name) {
+            messageApi.error("incorrect project name");
+            return false;
+          }
+          const deletePromise = deleteProjectReq(currentProject.id).then(
+            (res) => {
+              setDeleteModalShow(false);
+            },
+          );
+          toastPromise(deletePromise);
+        }}
+        onCancel={() => {
+          setDeleteModalShow(false);
+        }}
+      >
+        <div
+          style={{
+            lineHeight: "2em",
+          }}
+        >
+          Are you sure want to delete the project{" "}
+          <strong>{currentProject.name}</strong> ?
+          <br />
+          input the project name to confirm
+          <Input
+            value={projectNameInp}
+            onChange={(e) => {
+              setProjectNameInp(e.target.value);
+            }}
+            style={{
+              marginTop: "4px",
+            }}
+          />
+        </div>
+      </Modal>
       <ProjectEditor
         editorType={EditorType.MODIFY}
         projectM={modifyProject}
         onSubmit={onProjectEditorSubmit}
         updaterProjectM={updateModifyProject}
       />
-      <Button onClick={() => {
-
-        const deletePromise = deleteProjectReq(currentProject.id);
-        toastPromise(deletePromise);
-
-      }}>Delete Project</Button>
+      <Button
+        onClick={async () => {
+          setDeleteModalShow(true);
+        }}
+      >
+        Delete Project
+      </Button>
     </div>
   );
 };
