061dfa86c4cecd582b1c2485cddb4d650d612169
alin <a2231232@gmail.com>
2024-10-02 22:57:02 +0800

    add ws support

--- backEnd/src/action/ProxyAction.ts
+++ backEnd/src/action/ProxyAction.ts
@@ -9,6 +9,7 @@ import * as http from "http";
 import { isRecordBody } from "../util/bodyParseUtil";
 import typeis from "type-is";
 import { LogM } from "core/struct/log";
+import ws from "ws";
 
 const inflateAsync = util.promisify(zlib.inflate);
 const unzipAsync = util.promisify(zlib.unzip);
@@ -30,7 +31,7 @@ proxy.on("proxyReq", function (proxyReq, req, res, options) {
   if (proxyAction.requestHeaders && proxyAction.requestHeaders.length > 0) {
     proxyAction.requestHeaders.forEach(([headerName, headerValue]) => {
       // ignore the blank header name
-      if(headerName.trim() === ''){
+      if (headerName.trim() === "") {
         return;
       }
       proxyReq.setHeader(headerName, headerValue);
@@ -131,7 +132,7 @@ function handleExternalHeaders(
 ) {
   headers.forEach((item) => {
     // ignore the empty header
-    if(item[0].trim() === ''){
+    if (item[0].trim() === "") {
       return;
     }
     res.headers[item[0].toLowerCase()] = item[1];
@@ -181,6 +182,24 @@ export default class ProxyActionImpl implements IAction {
     res: express.Response,
     logM: LogM | undefined
   ): Promise<void> {
+    // handle websocket
+    if (
+      req.method === "GET" &&
+      req.headers.upgrade &&
+      req.headers.upgrade.toLowerCase() === "websocket"
+    ) {
+      // handle websocket
+      const wss = new ws.WebSocketServer({ noServer: true });
+
+      wss.on("connection", (ws) => {
+        ws.on("message", (message) => {
+          console.log(message);
+        });
+      });
+
+      const wsc = new ws.WebSocket(`ws://${this.action.host}`);
+    }
+
     // handle cross
     if (this.action.handleCross && req.method.toUpperCase() === "OPTIONS") {
       return handleOptionsCross(req, res, this.action.crossAllowCredentials);
